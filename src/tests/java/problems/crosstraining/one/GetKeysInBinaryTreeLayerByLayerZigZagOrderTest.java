ef5db1b45a4469d03be9e1af1a12534e3dcf7f71
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-10-28 13:11:12 -0700

    Oct 28 Updates cross training 1

---
+++ src/tests/java/problems/crosstraining/one/GetKeysInBinaryTreeLayerByLayerZigZagOrderTest.java
@@ -0,0 +1,40 @@
+package problems.crosstraining.one;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetKeysInBinaryTreeLayerByLayerZigZagOrderTest {
+    @Test
+    public void test_tree(){
+        TreeNode a = new TreeNode(5);
+        TreeNode b = new TreeNode(3);
+        TreeNode c = new TreeNode(8);
+        TreeNode d = new TreeNode(1);
+        TreeNode e = new TreeNode(4);
+        TreeNode f = new TreeNode(11);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+
+        List<Integer> expected = new ArrayList<>();
+        expected.add(5);
+        expected.add(3);
+        expected.add(8);
+        expected.add(11);
+        expected.add(4);
+        expected.add(1);
+
+
+        GetKeysInBinaryTreeLayerByLayerZigZagOrder gk = new GetKeysInBinaryTreeLayerByLayerZigZagOrder();
+        List<Integer> actual =  gk.zigZag(a);
+        assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+}
