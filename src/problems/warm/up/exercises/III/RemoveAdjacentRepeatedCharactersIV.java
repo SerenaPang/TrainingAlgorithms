43fb831044a683af822089d34ad19b0c1f8c212f
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-22 10:54:40 -0800

    Nov 22th cross training 3

--- src/problems/warm/up/exercises/III/RemoveAdjacentRepeatedCharactersIV.java
+++ src/problems/warm/up/exercises/III/RemoveAdjacentRepeatedCharactersIV.java
@@ -1,4 +1,35 @@
 package problems.warm.up.exercises.III;
 
 public class RemoveAdjacentRepeatedCharactersIV {
+    /**
+     * TC: O(n) where n is the length of input
+     * SC: O(n) where nn is the length of input
+     *
+     * two pointers scan from left to right
+     * using the index position of first pointer as the top of the stack
+     * */
+    public String deDup(String input) {
+      char[] array = input.toCharArray();
+      int slow = 0;
+      for (int fast = 1; fast < array.length; fast++) {
+          if (slow == -1 || array[slow] != array[fast]) {
+              slow++;
+             array[slow] = array[fast];
+          } else {
+              while (fast + 1 < array.length && array[fast] == array[fast + 1]) {
+                  fast++;
+              }
+              slow--;
+          }
+      }
+      return new String(array, 0, slow + 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "abbbaaccz";
+        String s1 = "aabccdc";
+        RemoveAdjacentRepeatedCharactersIV r = new RemoveAdjacentRepeatedCharactersIV();
+        System.out.println(s);
+        System.out.println(r.deDup(s));
+    }
 }
