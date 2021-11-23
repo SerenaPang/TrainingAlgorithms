1a61f222efab4b1e5ef2fcead0e0efefc4400743
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-23 11:18:10 -0800

    Nov 23th cross training 3

--- src/problems/crosstraining/three/CommonElementsInKSortedLists.java
+++ src/problems/crosstraining/three/CommonElementsInKSortedLists.java
@@ -4,13 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommonElementsInKSortedLists {
-
+    /**
+     * find common elements in the lists two by two
+     * TC: k * O(n), n is the len of each sorted list
+     * SC: O(n)
+     * */
     public List<Integer> commonElementsInKSortedLists(List<List<Integer>> input) {
-        List<Integer> result = new ArrayList<>();
-
+        List<Integer> result =  input.get(0);
+        for (int i = 1; i < input.size(); i++) {
+           result = commonElementsInTwoList(result, input.get(i));
+        }
         return result;
     }
+
+    public List<Integer> commonElementsInTwoList(List<Integer> one, List<Integer> two) {
+        List<Integer> result = new ArrayList<>();
+        int ai = 0;
+        int bi = 0;
+        while (ai < one.size() && bi < two.size()) {
+            if (one.get(ai) == two.get(bi)) {
+                result.add(one.get(ai));
+                ai++;
+                bi++;
+            } else if (one.get(ai) < two.get(bi)) {
+                ai++;
+            } else {
+                bi++;
+            }
+        }
+        return result;
+    }
+
     public void print(List<Integer> result) {
+        System.out.println("Printing list:");
         for (int num : result) {
             System.out.print(num + " ");
         }
@@ -19,7 +45,7 @@ public class CommonElementsInKSortedLists {
 
     public static void main(String[] args) {
         List<List<Integer>> input = new ArrayList<>();
-                //new ArrayList<>();
+
         List<Integer> a = new ArrayList<>();
         List<Integer> b = new ArrayList<>();
         List<Integer> c = new ArrayList<>();
@@ -38,9 +64,16 @@ public class CommonElementsInKSortedLists {
         c.add(2);
         c.add(4);
 
+        input.add(a);
+        input.add(b);
+        input.add(c);
+
         CommonElementsInKSortedLists common = new CommonElementsInKSortedLists();
+        common.print(a);
+        common.print(b);
+        common.print(c);
         List<Integer> result = common.commonElementsInKSortedLists(input);
-        System.out.println("The common elements in these 3 arrays are: ");
+        System.out.println("The common elements in these 3 lists are: ");
         common.print(result);
     }
 }
