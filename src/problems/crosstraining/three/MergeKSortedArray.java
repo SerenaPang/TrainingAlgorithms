26a20b9a4d22d8e507f1b621a569250db139c335
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-23 13:24:44 -0800

    Nov 23th cross training 3

--- src/problems/crosstraining/three/MergeKSortedArray.java
+++ src/problems/crosstraining/three/MergeKSortedArray.java
@@ -1,4 +1,31 @@
 package problems.crosstraining.three;
 
 public class MergeKSortedArray {
+
+    public int[] merge(int[][] arrayOfArrays) {
+        int[] result = new int[arrayOfArrays.length];
+
+
+        return result;
+    }
+
+    public void printTwoDimentionalArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MergeKSortedArray m = new MergeKSortedArray();
+        int[][] a = new int[][]{
+                {3},
+                {1,2,3,4,5}
+        };
+        m.printTwoDimentionalArr(a);
+
+    }
 }
