977ca71ab2c9ada5337f196870015e6b0f1653fe
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-24 11:24:59 -0800

    Nov 24th cross training 3

--- src/problems/crosstraining/three/MaxWaterTrappedII.java
+++ src/problems/crosstraining/three/MaxWaterTrappedII.java
@@ -1,4 +1,23 @@
 package problems.crosstraining.three;
 
 public class MaxWaterTrappedII {
+
+    public int maxTrapped(int[][] matrix) {
+        int result = 0;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                { 2, 3, 4, 2 },
+                { 3, 1, 2, 3 },
+                { 4, 3, 5, 4 }
+        };
+
+        MaxWaterTrappedII m = new MaxWaterTrappedII();
+        int result = m.maxTrapped(matrix);
+        System.out.println("The maximum water trapped in the array is: " + result);
+    }
+
 }
