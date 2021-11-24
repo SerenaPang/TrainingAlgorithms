de10be9956238f68a274d58e07d933e7d3d47bd7
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-24 10:19:20 -0800

    Nov 24th cross training 3

--- src/problems/crosstraining/three/MergeKSortedLists.java
+++ src/problems/crosstraining/three/MergeKSortedLists.java
@@ -1,7 +1,40 @@
 package problems.crosstraining.three;
 
-public class MergeKSortedLists {
-    public ListNode merge(List<ListNodde> listOfLists) {
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
+public class MergeKSortedLists {
+    /**
+     * TC: O(nlogk)
+     * SC: O(k)
+     * */
+    public ListNode merge(List<ListNode> listOfLists) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(11, new MyComparator());
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for (ListNode node : listOfLists) {
+            if (node != null) {
+                minHeap.offer(node);
+            }
+        }
+        while (!minHeap.isEmpty()) {
+            cur.next = minHeap.poll();
+            if (cur.next.next != null) {
+                minHeap.offer(cur.next.next);
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public static class MyComparator implements Comparator<ListNode> {
+        @Override
+        public int compare(ListNode o1, ListNode o2) {
+            if (o1.value == o2.value) {
+                return 0;
+            }
+            return o1.value < o2.value ? -1 : 1;
+        }
     }
 }
