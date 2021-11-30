1955c729729cc3296f46a819a4822ea04db5cde1
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-11-30 12:27:11 -0800

    cross-training 4

--- src/problems/crosstraining/IV/array/hashtable/linkedlist/math/MaximumValuesOfSizeKSlidingWindows.java
+++ src/problems/crosstraining/IV/array/hashtable/linkedlist/math/MaximumValuesOfSizeKSlidingWindows.java
@@ -1,9 +1,20 @@
 package problems.crosstraining.IV.array.hashtable.linkedlist.math;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * find the biggest number in all the k size sliding windows
  * */
 public class MaximumValuesOfSizeKSlidingWindows {
+    public List<Integer> maxWindows(int[] array, int k) {
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        return result;
+    }
 
     public static void main(String[] args) {
 
