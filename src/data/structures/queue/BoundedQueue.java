430770f027b2cc0a9f07e56f2a90c36da95f74e2
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-12-07 12:09:59 -0800

    data structure

--- src/data/structures/queue/BoundedQueue.java
+++ src/data/structures/queue/BoundedQueue.java
@@ -12,12 +12,19 @@ public class BoundedQueue {
     int tail;
     int size;
     Integer[] array;
+    //remember the head and tail is at the same position
     public BoundedQueue(int capacity) {
         array = new Integer[capacity];
         head = tail = 0;
         size = 0;
     }
-
+    /**
+     * when adding a new element to a circular queue,
+     * case 1: if the size is full, we are not allow to add more elements
+     * case 2:if the queue is not full
+     * case 2.1: if the position to add element is the tail, tail index has to set to 0
+     * case 2.2: if the position to add is not the tail, then increment the index
+     * */
     public boolean offer(Integer element) {
         if (size == array.length) {
             return false;
@@ -31,9 +38,36 @@ public class BoundedQueue {
         size++;
         return true;
     }
+    //check the first element of the queue
+    public Integer peek() {
+        if (size == 0) {
+            return null;
+        }
+        return array[head];
+    }
+    /**
+     * poll the last element of the queue out
+     * if we are polling the only element in the array, we need to set the head to be at position 0
+     * */
+    public Integer poll() {
+        if (size == 0) {
+            return null;
+        }
+        Integer result = array[head];
+        head = head + 1 == array.length ? 0 : head + 1;
+        size--;
+        return result;
+    }
 
+    public int size() {
+        return  size;
+    }
 
-    public boolean offerII(Integer element) {
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
+    public boolean isFull() {
+        return  size == array.length;
     }
 }
