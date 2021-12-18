9a13b59d877ecdbfd14d42f260b41d794318fcb8
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-12-17 20:08:10 -0800

    recursion

---
+++ src/problems/recursion/sorting/AtoThePowerOfb.java
@@ -0,0 +1,14 @@
+package problems.recursion.sorting;
+/**
+ *
+ *  TC:
+ *  SC:
+ *
+ * */
+public class AtoThePowerOfb {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 0;
+
+    }
+}
