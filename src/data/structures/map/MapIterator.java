13e6c0ad9958b987a53a80f3a480990f49ce489e
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-12-22 13:29:52 -0800

    iterator

--- src/data/structures/map/MapIterator.java
+++ src/data/structures/map/MapIterator.java
@@ -1,9 +1,12 @@
 package data.structures.map;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class MapIterator {
+    //use iterator
+    //we can also remove entries from the map during ieration by calling iterator.remove() method
     public void iterateMap(Map<Integer, Integer> map) {
         Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
@@ -12,4 +15,32 @@ public class MapIterator {
                     ", Value = " + entry.getValue());
         }
     }
+
+    //use for iteration
+    //most common
+    public void iterateMapII(Map<Integer, String> map){
+        //using keySet() for iteration over keys
+        for (Integer id : map.keySet()) {
+            System.out.println("key: " + id);
+        }
+        //using values() for iteration over values
+        for (String name : map.values()) {
+            System.out.println("value: " + name);
+        }
+    }
+    //use for iteration
+    //most common
+    public void iterateMapIII(Map<String, String> map) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+    }
+
+    //use lambda expression forEach(action) method to iterate map
+    //clean and fast
+    public void iterateMapIV(Map<String, String> map) {
+        map.forEach((k,v) -> System.out.println("key: " + k + " value: " + v));
+    }
+
+
 }
