c1b86333990c93fa78fd87d762a1c25afb87560f
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2021-12-28 20:50:25 -0800

    sorting

--- src/problems/recursion/sorting/RainbowSort.java
+++ src/problems/recursion/sorting/RainbowSort.java
@@ -2,29 +2,92 @@ package problems.recursion.sorting;
 
 public class RainbowSort {
     /**
-     * x: from 0 - i (not including i) are the first class of the element,
-     * y: from i (including i) to j - 1 (not including j) are the second class of the elements;
-     * z: from j to k(the end of the array) are the third class of the elements
-     *
-     * 0到 i前面都是1类数字（不包括i）， 从 i（包括i）到j（不包括j）是第二类数字，从j（包括j） 到k（包括k）是第三类数字
-     * i 和 k 相向而行
-     * 当后面的数字要往放的时候，如果是0-i区间，i和j都要往后走，k往前走
-     * 当前面的数字要往后面放的时候，如果是
-     * z y z x y x -> x x y y z
-     * i
-     * j
-     *           k
-     * swap i and k; i++, j++, k--
-     * x y z x y z
-     *   i
-     *   j
-     *         k
-     *
+     * TC: O(n)
+     * SC: O(n)
+     * create counters to count the number of -1, 0, and 1s. and then populate them in the result array
      * */
-    public int[] rainbowSort(int[] array) {
+    public int[] rainbowSortI(int[] array) {
+       int[] result = new int[array.length];
+       int neg = 0;
+       int zero = 0;
+       int one = 0;
+       for (int i = 0 ; i < array.length; i++) {
+           if (array[i] < 0) {
+               neg++;
+           } else if (array[i] == 0) {
+               zero++;
+           } else {
+               one++;
+           }
+       }
+       System.out.println("There are " + neg + " negative number," + zero + " zeros, and " + one + " ones.");
 
+        //populate result array
+        for (int j = 0; j < neg; j++) {
+           result[j] = -1;
+        }
+
+        for (int a = neg; a < (neg + zero); a++) {
+            result[a] = 0;
+        }
+
+        for (int b = (neg + zero); b < result.length; b++) {
+            result[b] = 1;
+        }
+       return result;
+    }
+    /**
+     * divide the array into 4 sections
+     * -1: 0 - i - 1
+     * 0: i - j - 1
+     * 1: j - k
+     * k - end
+
+     * i = 0
+     * j = 0
+     * k = array.length
+     * [j,k]:unexplored area
+     *
+     * -1 -1 -1 -1 -1 0 0 0 0 1 1 1 1 1 -1
+     *                i       j       k
+     *                neg     zero    one
+     * -1 0 1 1 1 -1 0 0 1 0 -1 1 -1 -1
+     *  i
+     *  j
+     *    k
+     *
+     * -1 0
+     *
+     * a b c c c a b b c b a c a a
+     *
+     * we swap the zero and one, and then we swap neg and zero
+     * TC: O(n)
+     * TC: O(1)
+     * */
+    public int[] rainbowSortII(int[] array) {
+        int neg = 0;
+        int zero = 0;
+        int one = array.length - 1;
+        while (zero < one) {
+            if (array[zero] == -1) {
+                swap(array, neg, zero);
+                neg++;
+                zero++;
+            } else if (array[zero] == 0) {
+                zero++;
+            } else {
+                swap(array, zero, one);
+                one--;
+            }
+        }
 
         return array;
+    }
+
+    public void swap(int[] array, int x, int y) {
+        int tmp = array[x];
+        array[x] = array[y];
+        array[y] = tmp;
     }
 
     public void print(int[] array) {
@@ -35,10 +98,12 @@ public class RainbowSort {
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{1,0,1,-1,0};
+        //int[] array = new int[]{1,0,1,-1,0};
+        int[] array = new int[]{-1, 0, 1, 1, 1, -1, 0, 0, 1, 0, -1, 1, -1, -1};
         RainbowSort r = new RainbowSort();
         r.print(array);
-        int[] result = r.rainbowSort(array);
+        //int[] result = r.rainbowSortI(array);
+        int[] result = r.rainbowSortII(array);
         r.print(result);
     }
 }
